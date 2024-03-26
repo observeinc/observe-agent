77a55c6a6c36016abf9ba22bc4ba7405fc4de926
Alex Lew <alex.lew@observeinc.com>
2024-03-26 14:39:07 -0700

    split out logic

---
+++ cmd/connections/otelcollector/collector.go
@@ -0,0 +1,95 @@
+package observeotel
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/confmap/provider/fileprovider"
+	"go.opentelemetry.io/collector/connector"
+	"go.opentelemetry.io/collector/connector/connectortest"
+	"go.opentelemetry.io/collector/exporter"
+	"go.opentelemetry.io/collector/exporter/debugexporter"
+	"go.opentelemetry.io/collector/extension"
+	"go.opentelemetry.io/collector/extension/extensiontest"
+	"go.opentelemetry.io/collector/otelcol"
+	collector "go.opentelemetry.io/collector/otelcol"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/processor/processortest"
+	"go.opentelemetry.io/collector/receiver"
+	"go.opentelemetry.io/collector/receiver/otlpreceiver"
+)
+
+func makeMapProvidersMap(providers ...confmap.Provider) map[string]confmap.Provider {
+	ret := make(map[string]confmap.Provider, len(providers))
+	for _, provider := range providers {
+		ret[provider.Scheme()] = provider
+	}
+	return ret
+}
+
+func generateCollectorSettings() *collector.CollectorSettings {
+	set := &collector.CollectorSettings{
+		BuildInfo: component.NewDefaultBuildInfo(),
+		Factories: baseFactories,
+		ConfigProviderSettings: collector.ConfigProviderSettings{
+			ResolverSettings: confmap.ResolverSettings{
+				URIs:      []string{filepath.Join("conf.d", "otel-collector.yaml")},
+				Providers: makeMapProvidersMap(fileprovider.NewWithSettings(confmap.ProviderSettings{})),
+			},
+		},
+	}
+	return set
+}
+
+func baseFactories() (otelcol.Factories, error) {
+	var factories otelcol.Factories
+	var err error
+
+	if factories.Extensions, err = extension.MakeFactoryMap(extensiontest.NewNopFactory()); err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	if factories.Receivers, err = receiver.MakeFactoryMap(otlpreceiver.NewFactory()); err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	if factories.Exporters, err = exporter.MakeFactoryMap(debugexporter.NewFactory()); err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	if factories.Processors, err = processor.MakeFactoryMap(processortest.NewNopFactory()); err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	if factories.Connectors, err = connector.MakeFactoryMap(connectortest.NewNopFactory()); err != nil {
+		return otelcol.Factories{}, err
+	}
+
+	return factories, err
+}
+
+func StartCollector(wg *sync.WaitGroup) error {
+	wg.Add(1)
+	ctx := context.Background()
+	set := generateCollectorSettings()
+	col, err := collector.NewCollector(*set)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to start agent: %v\n", err)
+		os.Exit(1)
+	}
+
+	colErrorChannel := make(chan error, 1)
+	// col.Run blocks until receiving a SIGTERM signal, so needs to be started
+	// asynchronously, but it will exit early if an error occurs on startup
+	go func() {
+		colErrorChannel <- col.Run(ctx)
+	}()
+
+	// wait until the collector server is in the Running state, or an error was returned
+	return <-colErrorChannel
+}
--- cmd/start.go
+++ cmd/start.go
@@ -4,29 +4,10 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"path/filepath"
+	observeotel "observe/agent/cmd/connections/otelcollector"
 	"sync"
-	"time"
 
 	"github.com/spf13/cobra"
-	component "go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/confmap"
-	"go.opentelemetry.io/collector/confmap/provider/fileprovider"
-	"go.opentelemetry.io/collector/connector"
-	"go.opentelemetry.io/collector/connector/connectortest"
-	"go.opentelemetry.io/collector/exporter"
-	"go.opentelemetry.io/collector/exporter/debugexporter"
-	"go.opentelemetry.io/collector/extension"
-	"go.opentelemetry.io/collector/extension/extensiontest"
-	"go.opentelemetry.io/collector/otelcol"
-	collector "go.opentelemetry.io/collector/otelcol"
-	"go.opentelemetry.io/collector/processor"
-	"go.opentelemetry.io/collector/processor/processortest"
-	"go.opentelemetry.io/collector/receiver"
-	"go.opentelemetry.io/collector/receiver/otlpreceiver"
 )
 
 // startCmd represents the start command
@@ -42,7 +23,7 @@ to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var wg sync.WaitGroup
 		wg.Wait()
-		return startCollector(wg)
+		return observeotel.StartCollector(&wg)
 	},
 }
 
@@ -58,119 +39,4 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// func generateConfig() *service.Config {
-// 	return &service.Config{
-// 		Telemetry: telemetry.Config{
-// 			Logs: telemetry.LogsConfig{
-// 				Level:             zapcore.DebugLevel,
-// 				Development:       true,
-// 				Encoding:          "console",
-// 				DisableCaller:     true,
-// 				DisableStacktrace: true,
-// 				OutputPaths:       []string{"stderr", "./output-logs"},
-// 				ErrorOutputPaths:  []string{"stderr", "./error-output-logs"},
-// 				InitialFields:     map[string]any{"fieldKey": "filed-value"},
-// 			},
-// 			Metrics: telemetry.MetricsConfig{
-// 				Level:   configtelemetry.LevelNormal,
-// 				Address: ":8080",
-// 			},
-// 		},
-// 		// Extensions: extensions.Config{component.MustNewID("nop")},
-// 		// Pipelines: pipelines.Config{
-// 		// 	component.MustNewID("traces"): {
-// 		// 		Receivers:  []component.ID{component.MustNewID("nop")},
-// 		// 		Processors: []component.ID{component.MustNewID("nop")},
-// 		// 		Exporters:  []component.ID{component.MustNewID("nop")},
-// 		// 	},
-// 		// },
-// 	}
-// }
-
-func startCollector(wg sync.WaitGroup) error {
-	wg.Add(1)
-	ctx := context.Background()
-	set := collector.CollectorSettings{
-		BuildInfo: component.NewDefaultBuildInfo(),
-		Factories: BaseFactories,
-		ConfigProviderSettings: collector.ConfigProviderSettings{
-			ResolverSettings: confmap.ResolverSettings{
-				URIs:      []string{filepath.Join("conf.d", "otel-collector.yaml")},
-				Providers: makeMapProvidersMap(fileprovider.NewWithSettings(confmap.ProviderSettings{})),
-			},
-		},
-	}
-	col, err := collector.NewCollector(set)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Println("We created our collector", col)
-	fmt.Println(col.GetState())
-	if err != nil {
-		fmt.Errorf("err: %e", err)
-	}
-	colErrorChannel := make(chan error, 1)
-
-	// col.Run blocks until receiving a SIGTERM signal, so needs to be started
-	// asynchronously, but it will exit early if an error occurs on startup
-	go func() {
-		colErrorChannel <- col.Run(ctx)
-	}()
-
-	// wait until the collector server is in the Running state
-	go func() {
-		for {
-			state := col.GetState()
-			fmt.Println("current state", state)
-			if state == collector.StateRunning {
-				// colErrorChannel <- nil
-				fmt.Println("wait group now done")
-				wg.Done()
-				break
-			}
-			time.Sleep(time.Millisecond * 200)
-		}
-	}()
-
-	// wait until the collector server is in the Running state, or an error was returned
-	return <-colErrorChannel
-}
-
-// NopFactories returns a otelcol.Factories with all nop factories.
-func BaseFactories() (otelcol.Factories, error) {
-	var factories otelcol.Factories
-	var err error
-
-	if factories.Extensions, err = extension.MakeFactoryMap(extensiontest.NewNopFactory()); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Receivers, err = receiver.MakeFactoryMap(otlpreceiver.NewFactory()); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Exporters, err = exporter.MakeFactoryMap(debugexporter.NewFactory()); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Processors, err = processor.MakeFactoryMap(processortest.NewNopFactory()); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Connectors, err = connector.MakeFactoryMap(connectortest.NewNopFactory()); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	return factories, err
-}
-
-func makeMapProvidersMap(providers ...confmap.Provider) map[string]confmap.Provider {
-	ret := make(map[string]confmap.Provider, len(providers))
-	for _, provider := range providers {
-		ret[provider.Scheme()] = provider
-	}
-	return ret
 }
