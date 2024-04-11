049bb2a2b656d53307fcdc04af3cb2d3125ecbf8
Alex Lew <alex.lew@observeinc.com>
2024-04-11 12:00:58 -0700

    feat: add file_storage permissions to preinstall script

--- packaging/linux/scripts/preinstall.sh
+++ packaging/linux/scripts/preinstall.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 getent passwd observe-agent >/dev/null || useradd --system --user-group --no-create-home --shell /sbin/nologin observe-agent
+
+sudo mkdir /var/lib/otelcol
+sudo mkdir /var/lib/otelcol/file_storage
+sudo chown -R observe-agent /var/lib/otelcol/file_storage
