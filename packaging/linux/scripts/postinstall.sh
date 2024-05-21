c7c173cd055f0e9ec4d6435a9eb7fa9c49b2a3ff
obs-gh-alexlew <153021320+obs-gh-alexlew@users.noreply.github.com>
2024-05-21 13:21:18 -0700

    Rm duplicates

--- packaging/linux/scripts/postinstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo setcap 'cap_dac_read_search=ep' /usr/bin/observe-agent
--- packaging/linux/scripts/preremove.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if command -v systemctl >/dev/null 2>&1; then
-    systemctl stop observe-agent.service
-    systemctl disable observe-agent.service
-fi
