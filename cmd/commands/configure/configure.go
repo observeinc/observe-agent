736bd8832cdeeb19b69a3a8da1236acaa96802ed
Alex Lew <alex.lew@observeinc.com>
2024-05-22 13:09:30 -0700

    .

--- cmd/commands/configure/configure.go
+++ cmd/commands/configure/configure.go
@@ -4,21 +4,57 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package configure
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
 	"observe/agent/cmd"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var (
+	token       string
+	observe_url string
+	//go:embed observe-agent.tmpl
+	configTemplateFS embed.FS
+)
+
+const configTemplate = "observe-agent.tmpl"
+
+type AgentConfig struct {
+	Token      string `yaml:"token"`
+	ObserveURL string `yaml:"observe_url"`
+}
 
 var configureCmd = &cobra.Command{
 	Use:   "configure",
 	Short: "Configure agent",
 	Long:  `This command takes in parameters and writes them to the observe agent's configuration file.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("Running configuration...\n\n")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		configValues := AgentConfig{
+			Token:      viper.GetString("token"),
+			ObserveURL: viper.GetString("observe_url"),
+		}
+		f, err := os.Create(viper.ConfigFileUsed())
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		t := template.Must(template.New(configTemplate).ParseFS(configTemplateFS, configTemplate))
+		if err := t.ExecuteTemplate(f, configTemplate, configValues); err != nil {
+			return err
+		}
+		fmt.Print("Writing configuration values...\n\n")
+		return nil
 	},
 }
 
 func init() {
+	configureCmd.PersistentFlags().StringVar(&token, "token", "", "Observe token")
+	configureCmd.PersistentFlags().StringVar(&observe_url, "observe_url", "", "Observe data collection url")
 	cmd.RootCmd.AddCommand(configureCmd)
+	viper.BindPFlag("token", configureCmd.PersistentFlags().Lookup("token"))
+	viper.BindPFlag("observe_url", configureCmd.PersistentFlags().Lookup("observe_url"))
 }
