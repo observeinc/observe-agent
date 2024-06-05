cc17e7b6e28ac563ef5af68bc82d6d0fd06e1a97
Alex Lew <alex.lew@observeinc.com>
2024-06-05 13:16:15 -0700

    .

--- packaging/linux/scripts/preinstall.sh
+++ packaging/linux/scripts/preinstall.sh
@@ -2,7 +2,7 @@
 
 # Set up user and permissions
 getent passwd observe-agent >/dev/null || useradd --system --user-group --no-create-home --shell /sbin/nologin observe-agent
-sudo adduser observe-agent systemd-journal
+sudo usermod -a -G systemd-journal observe-agent
 
 sudo mkdir -p /var/lib/observe-agent/filestorage
 sudo chown observe-agent /var/lib/observe-agent/filestorage
