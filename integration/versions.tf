ca1091d093202fbe4f6036787994db4254c73632
Nikhil Dua <nikhil.dua@observeinc.com>
2024-06-11 13:47:47 -0700

    update versions.tf

--- integration/versions.tf
+++ integration/versions.tf
@@ -4,21 +4,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.11"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.11.2"
-    }
-
+    }   
   }
   required_version = ">= 1.2"
-
-
 }
 
-provider "time" {
-  # Configuration options
-}
 provider "aws" {
   region  = "us-west-1" # Specify the AWS region
   profile = "blunderdome"
