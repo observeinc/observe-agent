d53b45af07bd8a674f38e83812eee7e6eed7d62d
Nikhil Dua <nikhil.dua@observeinc.com>
2024-06-26 14:24:11 -0700

    fmting

--- integration/modules/create_ec2/outputs.tf
+++ integration/modules/create_ec2/outputs.tf
@@ -1,6 +1,7 @@
 locals {
   machine_config_list = formatlist("%s:%s", keys(local.AWS_MACHINE_CONFIGS[var.AWS_MACHINE]), values(local.AWS_MACHINE_CONFIGS[var.AWS_MACHINE]))
 }
+
 output "machine_name" {
   value = var.AWS_MACHINE
 }
@@ -22,15 +23,14 @@ output "instance_id" {
   value = aws_instance.observe_agent_instance.id
 
 }
+
 output "instance_state" {
   value = aws_instance.observe_agent_instance.instance_state
 }
 
-
 output "public_ip" {
   value = aws_instance.observe_agent_instance.public_ip
 }
-
 
 output "private_key_path" {
   value = var.PRIVATE_KEY_PATH
@@ -40,3 +40,4 @@ output "public_ssh_link" {
   value = "ssh -i ${var.PRIVATE_KEY_PATH} ${local.AWS_MACHINE_CONFIGS[var.AWS_MACHINE].default_user}@${aws_instance.observe_agent_instance.public_ip}"
 
 }
+
