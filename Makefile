6aa12179aaf6ea206132a1374c9906a10a23d18f
Alex Lew <alex.lew@observeinc.com>
2024-07-15 16:20:02 -0700

    feat: automate adding new deps via ocb

--- Makefile
+++ Makefile
@@ -6,3 +6,12 @@ go-test:
 ## release: Releases current tag through goreleaser
 release:
 	goreleaser release --clean
+
+## build-ocb: Builds observeotel binary
+build-ocb:
+	$(HOME)/bin/ocb --skip-compilation --config=builder-config.yaml
+	sed -i -e 's/package main/package observeotel/g' observe-agent/components.go
+	cp ./observe-agent/components.go ./cmd/collector/components.go
+	modmerge go.mod observe-agent/go.mod
+	go mod tidy
+	go mod vendor 
--- cmd/collector/otelcollector.go
+++ cmd/collector/otelcollector.go
@@ -3,31 +3,6 @@ package observeotel
 import (
 	"observe/agent/build"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/connector/countconnector"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/fileexporter"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/storage/filestorage"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/attributesprocessor"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/k8sattributesprocessor"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourcedetectionprocessor"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/awsecscontainermetricsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/dockerstatsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/elasticsearchreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filelogreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/filestatsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/iisreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/journaldreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sclusterreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/k8sobjectsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kafkametricsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kafkareceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/kubeletstatsreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/redisreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/statsdreceiver"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/windowseventlogreceiver"
 	"github.com/spf13/cobra"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap"
@@ -36,21 +11,9 @@ import (
 	"go.opentelemetry.io/collector/confmap/provider/httpprovider"
 	"go.opentelemetry.io/collector/confmap/provider/httpsprovider"
 	"go.opentelemetry.io/collector/confmap/provider/yamlprovider"
-	"go.opentelemetry.io/collector/connector"
-	"go.opentelemetry.io/collector/exporter"
-	"go.opentelemetry.io/collector/exporter/debugexporter"
-	"go.opentelemetry.io/collector/exporter/loggingexporter"
-	"go.opentelemetry.io/collector/exporter/otlphttpexporter"
-	"go.opentelemetry.io/collector/extension"
-	"go.opentelemetry.io/collector/extension/zpagesextension"
-	"go.opentelemetry.io/collector/processor/batchprocessor"
-	"go.opentelemetry.io/collector/processor/memorylimiterprocessor"
 
 	"go.opentelemetry.io/collector/otelcol"
 	collector "go.opentelemetry.io/collector/otelcol"
-	"go.opentelemetry.io/collector/processor"
-	"go.opentelemetry.io/collector/receiver"
-	"go.opentelemetry.io/collector/receiver/otlpreceiver"
 )
 
 func makeMapProvidersMap(providers ...confmap.Provider) map[string]confmap.Provider {
@@ -69,7 +32,7 @@ func GenerateCollectorSettings(URIs []string) *collector.CollectorSettings {
 	}
 	set := &collector.CollectorSettings{
 		BuildInfo: buildInfo,
-		Factories: baseFactories,
+		Factories: components,
 		ConfigProviderSettings: collector.ConfigProviderSettings{
 			ResolverSettings: confmap.ResolverSettings{
 				URIs: URIs,
@@ -84,69 +47,6 @@ func GenerateCollectorSettings(URIs []string) *collector.CollectorSettings {
 		},
 	}
 	return set
-}
-
-// Each module's factories needs to be manually included here for the parser to then handle that config.
-func baseFactories() (otelcol.Factories, error) {
-	var factories otelcol.Factories
-	var err error
-
-	if factories.Extensions, err = extension.MakeFactoryMap(
-		healthcheckextension.NewFactory(),
-		filestorage.NewFactory(),
-		zpagesextension.NewFactory(),
-	); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Receivers, err = receiver.MakeFactoryMap(
-		awsecscontainermetricsreceiver.NewFactory(),
-		dockerstatsreceiver.NewFactory(),
-		elasticsearchreceiver.NewFactory(),
-		filelogreceiver.NewFactory(),
-		filestatsreceiver.NewFactory(),
-		hostmetricsreceiver.NewFactory(),
-		iisreceiver.NewFactory(),
-		journaldreceiver.NewFactory(),
-		k8sclusterreceiver.NewFactory(),
-		k8sobjectsreceiver.NewFactory(),
-		kafkametricsreceiver.NewFactory(),
-		kafkareceiver.NewFactory(),
-		kubeletstatsreceiver.NewFactory(),
-		otlpreceiver.NewFactory(),
-		prometheusreceiver.NewFactory(),
-		redisreceiver.NewFactory(),
-		statsdreceiver.NewFactory(),
-		windowseventlogreceiver.NewFactory(),
-	); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Exporters, err = exporter.MakeFactoryMap(
-		loggingexporter.NewFactory(),
-		debugexporter.NewFactory(),
-		otlphttpexporter.NewFactory(),
-		fileexporter.NewFactory(),
-	); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Processors, err = processor.MakeFactoryMap(
-		attributesprocessor.NewFactory(),
-		batchprocessor.NewFactory(),
-		memorylimiterprocessor.NewFactory(),
-		k8sattributesprocessor.NewFactory(),
-		resourcedetectionprocessor.NewFactory(),
-		transformprocessor.NewFactory(),
-	); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	if factories.Connectors, err = connector.MakeFactoryMap(countconnector.NewFactory()); err != nil {
-		return otelcol.Factories{}, err
-	}
-
-	return factories, err
 }
 
 func GetOtelCollectorCommand(otelconfig *collector.CollectorSettings) *cobra.Command {
