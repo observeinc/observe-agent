c3f1a4c7b3d5e568a8da3fd0ffc6a1e9f4f2afaf
alex.lew <alex.lew@observeinc.com>
2024-07-18 00:27:40 +0000

    initial commit for k8s attributes processor

---
+++ cmd/components/processors/observek8sattributesprocessor/factory.go
@@ -0,0 +1,22 @@
+package observek8sattributesprocessor
+
+import (
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/processor"
+
+	"observe/agent/cmd/components/processors/observek8sattributesprocessor/internal/metadata"
+)
+
+var processorCapabilities = consumer.Capabilities{MutatesData: true}
+
+func NewFactory() processor.Factory {
+	return processor.NewFactory(
+		metadata.Type,
+		createDefaultConfig,
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{}
+}
