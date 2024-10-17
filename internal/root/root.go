90b57f3da80202eb68ebf69a17576c6295e146f3
Matt Cotter <matthew.cotter@observeinc.com>
2024-10-17 15:40:25 -0500

    fix: add a description to the root command

--- internal/root/root.go
+++ internal/root/root.go
@@ -17,16 +17,9 @@ var CfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "observe-agent",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A runner for the Observe agent process along with CLI utils.",
+	Long: `A runner for the Observe agent process along with CLI utils to help with setup
+and maintenance. To start the agent, run: observe-agent start`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
