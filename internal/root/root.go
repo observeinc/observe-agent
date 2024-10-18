9b75d11e06f8801ee7b41c24cc4573db1b2fef7d
obs-gh-mattcotter <matthew.cotter@observeinc.com>
2024-10-18 12:53:06 -0500

    fix: add a description to the root command (#108)
    ### Description
    
    OB-37820 update the short and long command descriptions for the agent room command

--- internal/root/root.go
+++ internal/root/root.go
@@ -17,16 +17,9 @@ var CfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "observe-agent",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Observe distribution of OTEL Collector",
+	Long: `Observe distribution of OTEL Collector along with CLI utils to help with setup
+and maintenance. To start the agent, run: observe-agent start`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
