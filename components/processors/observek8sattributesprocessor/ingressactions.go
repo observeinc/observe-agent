0d2a48840afef3d70185894b8c82bc8c6673da2d
Enrico Giorio <162458291+obs-gh-enricogiorio@users.noreply.github.com>
2024-10-24 09:33:31 +0200

    feat: Improve generation of Ingress rules (#112)
    We used to only return which host(s) each rule would apply to.
    
    This PR enhances the formatting of ingress rules, returning them as a
    JSON object with more information, such as the URL path they are defined
    on and which serviceName:port the rules are for.
    
    ### Description
    
    OB-XXX Please explain the changes you made here.
    
    ### Checklist
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary

--- components/processors/observek8sattributesprocessor/ingressactions.go
+++ components/processors/observek8sattributesprocessor/ingressactions.go
@@ -10,18 +10,88 @@ import (
 const (
 	IngressRulesAttributeKey        = "rules"
 	IngressLoadBalancerAttributeKey = "loadBalancer"
+	hostKey                         = "host"
+	rulesKey                        = "rules"
+	httpRulesKey                    = "httpRules"
+	pathKey                         = "path"
+	backendKey                      = "backend"
+	serviceKey                      = "service"
+	resourceKey                     = "resource"
+	portKey                         = "port"
+	nameKey                         = "name"
 )
 
-// Adapted from https://github.com/kubernetes/kubernetes/blob/0d3b859af81e6a5f869a7766c8d45afd1c600b04/pkg/printers/internalversion/printers.go#L1373
-func formatIngressRules(rules []netv1.IngressRule) string {
-	list := []string{}
+// formatIngressRules converts a slice of IngressRules to a minimal JSON representation.
+// The json structure is:
+//
+//		{
+//		  "host": "example.com",    (or "*" if no host is specified)
+//		  "rules": [
+//		    {
+//		      "path": "/app1",
+//		      "backend": {
+//		        "service": {        (inside "backend" could be either "service" or "resource")
+//		          "name": "app1-service",
+//		          "port": 8080       (could be either a number or a string)
+//		        }
+//		      }
+//		    },
+//		    {
+//		      "path": "/app2",
+//		      "backend": {
+//		        "resource": "app2-resource",   (alternative to service)
+//	            (no port here, just the name of the resource)
+//		      }
+//		    }
+//		  ]
+//		}
+func formatIngressRules(rules []netv1.IngressRule) []attributes {
+	var ret []attributes
+
 	for _, rule := range rules {
-		list = append(list, rule.Host)
+		host := rule.Host
+		if host == "" {
+			host = "*"
+		}
+
+		var httpRules []attributes
+		if rule.HTTP != nil {
+			for _, path := range rule.HTTP.Paths {
+
+				ruleInfo := attributes{
+					pathKey: path.Path,
+				}
+				backend := path.Backend
+				backendAttrs := attributes{}
+				if backend.Service != nil {
+					service := backend.Service
+					serviceAttrs := attributes{
+						nameKey: service.Name,
+					}
+					// Remove one level of indentation and use either the port
+					// name or the number as "port" directly inside "service",
+					// since we can use values of any type.
+					if service.Port.Name != "" {
+						serviceAttrs[portKey] = service.Port.Name
+					} else {
+						serviceAttrs[portKey] = service.Port.Number
+					}
+					backendAttrs[serviceKey] = serviceAttrs
+				} else {
+					backendAttrs[resourceKey] = backend.Resource.Name
+				}
+				ruleInfo[backendKey] = backendAttrs
+
+				httpRules = append(httpRules, ruleInfo)
+			}
+		}
+
+		ret = append(ret, attributes{
+			hostKey:      host,
+			httpRulesKey: httpRules,
+		})
 	}
-	if len(list) == 0 {
-		return "*"
-	}
-	ret := strings.Join(list, ",")
+
 	return ret
 }
 
@@ -35,7 +105,8 @@ func NewIngressRulesAction() IngressRulesAction {
 
 // Generates the Ingress "rules" facet.
 func (IngressRulesAction) ComputeAttributes(ingress netv1.Ingress) (attributes, error) {
-	return attributes{IngressRulesAttributeKey: formatIngressRules(ingress.Spec.Rules)}, nil
+	rules := formatIngressRules(ingress.Spec.Rules)
+	return attributes{IngressRulesAttributeKey: rules}, nil
 }
 
 // ---------------------------------- Ingress "loadBalancer" ----------------------------------
