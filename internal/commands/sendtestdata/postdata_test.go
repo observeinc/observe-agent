3fb80188e7347d92f0f245a505b7558a448883d4
Matt Cotter <matthew.cotter@observeinc.com>
2024-10-24 12:22:20 -0500

    add test for helper as well

--- internal/commands/sendtestdata/postdata_test.go
+++ internal/commands/sendtestdata/postdata_test.go
@@ -12,6 +12,28 @@ func TestPostTestData(t *testing.T) {
 	httpmock.Activate()
 	t.Cleanup(httpmock.DeactivateAndReset)
 
+	testURL := "https://example.com/test"
+	expectedResponse := `{"ok":true}`
+	// Verify that the data is sent to the expected endpoint along with the bearer and json headers.
+	httpmock.RegisterMatcherResponder("POST", testURL,
+		httpmock.BodyContainsString(`"hello":"world"`).And(
+			httpmock.HeaderIs("Content-Type", "application/json"),
+			httpmock.HeaderIs("SomeHeader", "some value"),
+		),
+		httpmock.NewStringResponder(200, expectedResponse),
+	)
+
+	testData := map[string]string{"hello": "world"}
+	testHeaders := map[string]string{"SomeHeader": "some value"}
+	resp, err := PostTestData(testData, testURL, testHeaders)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedResponse, resp)
+}
+
+func TestPostTestDataToObserve(t *testing.T) {
+	httpmock.Activate()
+	t.Cleanup(httpmock.DeactivateAndReset)
+
 	expectedResponse := `{"ok":true}`
 	// Verify that the data is sent to the expected endpoint along with the bearer and json headers.
 	httpmock.RegisterMatcherResponder("POST", "https://123456.collect.observe-eng.com/v1/http/test",
