2b6700f97ee675dc6e6cafde9d1c40504b624a0b
obs-gh-nikhildua <124205220+obs-gh-nikhildua@users.noreply.github.com>
2025-02-10 12:44:51 -0800

    Update windows amis

--- integration/modules/create_ec2/locals.tf
+++ integration/modules/create_ec2/locals.tf
@@ -89,7 +89,7 @@ locals {
 
     WINDOWS_SERVER_2016_BASE = {
       ami_instance_type = "t3.small"
-      ami_id            = "ami-0dbedda2d0007250e"
+      ami_id            = "ami-02e77d98d01fe65cd"
       ami_description   = "Microsoft Windows Server 2016 with Desktop Experience Locale English AMI provided by Amazon"
       default_user      = "Administrator"
       sleep             = 120
@@ -102,7 +102,7 @@ locals {
 
     WINDOWS_SERVER_2019_BASE = {
       ami_instance_type = "t3.small"
-      ami_id            = "ami-07b70ee6215b30557"
+      ami_id            = "ami-0965ccda58d4f713f"
       ami_description   = "Microsoft Windows Server 2019 with Desktop Experience Locale English AMI provided by Amazon"
       default_user      = "Administrator"
       sleep             = 120
@@ -115,7 +115,7 @@ locals {
   
      WINDOWS_SERVER_2022_BASE = {
       ami_instance_type = "t3.small"
-      ami_id            = "ami-02aa70527c24523ed"
+      ami_id            = "ami-055a418b44c502d59"
       ami_description   = "Microsoft Windows Server 2022 Full Locale English AMI provided by Amazon"
       default_user      = "Administrator"
       sleep             = 120
