observeinc/observe-agent

commit 97fbd0075bae0c238fa2e4f41a11c112c78800fd

refactor: split main otel config into multiple connections, add tests (#186)
### Description

OB-36266: Split main otel config into multiple connections. Use the
agent config object as the source of truth instead of viper. Add tests.

I also tested this with the new `observe-agent config --render-otel`
command. I enabled all options in my agent config, and diffed the output
between `main` and this branch and only these lines changed:
```diff
104c104
<             - /usr/local/observe-agent/config/otel-collector.yaml
---
>             - /usr/local/observe-agent/connections/common/base.yaml.tmpl
123c123
<         include: /usr/local/observe-agent/config/otel-collector.yaml
---
>         include: /usr/local/observe-agent/connections/common/base.yaml.tmpl
```
This is expected since the otel-collector.yaml file no longer exists.

### Checklist
- [x] Created tests which fail without the change (if possible)

scripts/install.ps1
@@ -21,7 +21,6 @@ $temp_dir="C:\temp"
 
 New-Item -ItemType Directory -Force -Path $temp_dir
 New-Item -ItemType Directory -Force -Path $observeagent_install_dir
-New-Item -ItemType Directory -Force -Path $observeagent_install_dir\config
 New-Item -ItemType Directory -Force -Path $observeagent_install_dir\connections
 New-Item -ItemType Directory -Force -Path $program_data_filestorage
 
@@ -34,7 +33,6 @@ if((Get-Service ObserveAgent -ErrorAction SilentlyContinue)){
 
 Expand-Archive -Force -LiteralPath $local_installer -DestinationPath "$temp_dir\observe-agent_Windows_x86_64"
 Copy-Item -Force -Path $temp_dir\observe-agent_Windows_x86_64\observe-agent.exe -Destination $observeagent_install_dir
-Copy-Item -Force -Path $temp_dir\observe-agent_Windows_x86_64\otel-collector.yaml -Destination $observeagent_install_dir\config\otel-collector.yaml
 Copy-Item -Force -Path $temp_dir\observe-agent_Windows_x86_64\connections\* -Destination $observeagent_install_dir\connections -Recurse
 
 # If there's already an observe-agent.yaml we don't copy the template from the downloaded installation package and leave existing observe-agent.yaml alone

scripts/install_linux.sh
@@ -93,8 +93,7 @@ sudo chmod +rw /var/lib/observe-agent/filestorage
 sudo cp -f $tmp_dir/observe-agent $agent_binary_path
 
 # Copy all config files to the proper dir.
-sudo cp -f $tmp_dir/otel-collector.yaml $observeagent_config_dir/otel-collector.yaml
-sudo rm -rf $observeagent_config_dir/connections
+sudo rm -rf $observeagent_config_dir/otel-collector.yaml $observeagent_config_dir/connections
 sudo cp -fR $tmp_dir/connections $observeagent_config_dir/connections
 sudo chown -R root:root $observeagent_config_dir
 

scripts/install_mac.sh
@@ -101,7 +101,6 @@ sudo chmod +rw /var/lib/observe-agent/filestorage
 # Copy all files to the install dir.
 sudo rm -rf $observeagent_install_dir/config $observeagent_install_dir/connections $observeagent_install_dir/observe-agent
 sudo cp $tmp_dir/observe-agent $observeagent_install_dir/observe-agent
-sudo cp -R $tmp_dir/config $observeagent_install_dir/config
 sudo cp -R $tmp_dir/connections $observeagent_install_dir/connections
 sudo chown -R root:wheel $observeagent_install_dir
 

commit 71f1dc4d3f6a26bd82b3934a39dc22955af0530d

fix: make install scripts backwards compatible (#190)
### Description

Make install scripts backwards compatible. This was accidentally broken
in 97fbd007.

scripts/install.ps1
@@ -21,6 +21,7 @@ $temp_dir="C:\temp"
 
 New-Item -ItemType Directory -Force -Path $temp_dir
 New-Item -ItemType Directory -Force -Path $observeagent_install_dir
+New-Item -ItemType Directory -Force -Path $observeagent_install_dir\config
 New-Item -ItemType Directory -Force -Path $observeagent_install_dir\connections
 New-Item -ItemType Directory -Force -Path $program_data_filestorage
 
@@ -33,6 +34,9 @@ if((Get-Service ObserveAgent -ErrorAction SilentlyContinue)){
 
 Expand-Archive -Force -LiteralPath $local_installer -DestinationPath "$temp_dir\observe-agent_Windows_x86_64"
 Copy-Item -Force -Path $temp_dir\observe-agent_Windows_x86_64\observe-agent.exe -Destination $observeagent_install_dir
+if (Test-Path $temp_dir\observe-agent_Windows_x86_64\otel-collector.yaml) {
+    Copy-Item -Force -Path $temp_dir\observe-agent_Windows_x86_64\otel-collector.yaml -Destination $observeagent_install_dir\config\otel-collector.yaml
+}
 Copy-Item -Force -Path $temp_dir\observe-agent_Windows_x86_64\connections\* -Destination $observeagent_install_dir\connections -Recurse
 
 # If there's already an observe-agent.yaml we don't copy the template from the downloaded installation package and leave existing observe-agent.yaml alone

scripts/install_linux.sh
@@ -94,6 +94,9 @@ sudo cp -f $tmp_dir/observe-agent $agent_binary_path
 
 # Copy all config files to the proper dir.
 sudo rm -rf $observeagent_config_dir/otel-collector.yaml $observeagent_config_dir/connections
+if [ -f $tmp_dir/otel-collector.yaml ]; then
+    sudo cp -f $tmp_dir/otel-collector.yaml $observeagent_config_dir/otel-collector.yaml
+fi
 sudo cp -fR $tmp_dir/connections $observeagent_config_dir/connections
 sudo chown -R root:root $observeagent_config_dir
 

scripts/install_mac.sh
@@ -101,6 +101,9 @@ sudo chmod +rw /var/lib/observe-agent/filestorage
 # Copy all files to the install dir.
 sudo rm -rf $observeagent_install_dir/config $observeagent_install_dir/connections $observeagent_install_dir/observe-agent
 sudo cp $tmp_dir/observe-agent $observeagent_install_dir/observe-agent
+if [ -d $tmp_dir/config ]; then
+    sudo cp -R $tmp_dir/config $observeagent_install_dir/config
+fi
 sudo cp -R $tmp_dir/connections $observeagent_install_dir/connections
 sudo chown -R root:wheel $observeagent_install_dir
 
71f1dc4d3f6a26bd82b3934a39dc22955af0530d
Matt Cotter <matthew.cotter@observeinc.com>
2025-04-21 14:41:12 -0500

    fix: make install scripts backwards compatible (#190)
    ### Description
    
    Make install scripts backwards compatible. This was accidentally broken
    in 97fbd007.

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -94,6 +94,9 @@ sudo cp -f $tmp_dir/observe-agent $agent_binary_path
 
 # Copy all config files to the proper dir.
 sudo rm -rf $observeagent_config_dir/otel-collector.yaml $observeagent_config_dir/connections
+if [ -f $tmp_dir/otel-collector.yaml ]; then
+    sudo cp -f $tmp_dir/otel-collector.yaml $observeagent_config_dir/otel-collector.yaml
+fi
 sudo cp -fR $tmp_dir/connections $observeagent_config_dir/connections
 sudo chown -R root:root $observeagent_config_dir
 
