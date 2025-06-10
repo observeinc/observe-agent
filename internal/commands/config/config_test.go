0ecb86e40f362458851b4d86fd850c1c8c5c0066
Matt Cotter <matthew.cotter@observeinc.com>
2025-06-10 10:53:23 -0500

    add option to set config dir, enable unit test

--- internal/commands/config/config_test.go
+++ internal/commands/config/config_test.go
@@ -10,9 +10,11 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/observeinc/observe-agent/internal/commands/util/logger"
+	"github.com/observeinc/observe-agent/internal/connections"
 	"github.com/observeinc/observe-agent/internal/root"
 	"github.com/observeinc/observe-agent/observecol"
 	"github.com/spf13/pflag"
@@ -20,12 +22,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO enable this test once the agent bundled configs don't depend on the host filesystem having the templates.
-func XTest_RenderOtelConfig(t *testing.T) {
+// TODO rework this test to handle our snapshot tests as go unit tests.
+func Test_RenderOtelConfig(t *testing.T) {
 	// Get current path
 	_, filename, _, ok := runtime.Caller(0)
 	assert.True(t, ok)
 	curPath := path.Dir(filename)
+
+	// Set the template base dir for all connections
+	for _, conn := range connections.AllConnectionTypes {
+		conn.ApplyOptions(connections.WithConfigFolderPath(filepath.Join(curPath, "../../../packaging/macos/connections")))
+	}
 
 	// Set config flags
 	flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
@@ -41,5 +48,5 @@ func XTest_RenderOtelConfig(t *testing.T) {
 	PrintShortOtelConfig(ctx, &output)
 	expected, err := os.ReadFile(filepath.Join(curPath, "test/output.yaml"))
 	assert.NoError(t, err)
-	assert.Equal(t, string(expected), output.String())
+	assert.Equal(t, strings.TrimSpace(string(expected)), strings.TrimSpace(output.String()))
 }
