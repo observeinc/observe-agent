3289167856cc37322accd4d5021aa7bcdc3973f9
obs-gh-alexlew <153021320+obs-gh-alexlew@users.noreply.github.com>
2025-09-10 14:23:16 -0700

    feat: Add heartbeat receiver configs (#257)
    OB-XXX Please explain the changes you made here.
    
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary

--- internal/connections/bundledconfig/docker/templates.go
+++ internal/connections/bundledconfig/docker/templates.go
@@ -13,4 +13,6 @@ var (
 	ProcessMetricsTemplateFS embed.FS
 	//go:embed self_monitoring/logs_and_metrics.yaml.tmpl
 	LogsAndMetricsTemplateFS embed.FS
+	//go:embed fleet/heartbeat_receiver.yaml.tmpl
+	HeartbeatTemplateFS embed.FS
 )
